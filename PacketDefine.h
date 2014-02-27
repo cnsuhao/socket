b3b6a24e6726f8be1d2e7a8964511cad4fa1847b
Administrator <Administrator@LAGWZFAH58CBH77>
2014-02-27 20:43:39 +0800

    数据包列表

---
+++ PacketDefine.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   PacketDefine.h
+ * Author: Vicky.H
+ * Email:  eclipser@163.com
+ *
+ * Created on 2014年2月27日, 下午8:41
+ */
+
+#ifndef CN_VICKY__PACKETDEFINE_H
+#define	CN_VICKY__PACKETDEFINE_H
+
+
+namespace Packet {
+
+    enum PACKET_DEF {
+        PACKET_NONE = 0,                                                        //0，空
+        PACKET_CG_CHAT,                                                         //聊天
+        
+    };
+}
+
+
+#endif	/* CN_VICKY__PACKETDEFINE_H */
+
